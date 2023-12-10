662682fb65157fa8e79b31b357901e69b2c4e522
ulugbe7 <ulugbekyusupov272@gmail.com>
2023-12-11 01:01:26 +0500

    finish: loops

--- src/main/java/com/epam/loops/MultiplicationTable.java
+++ src/main/java/com/epam/loops/MultiplicationTable.java
@@ -3,6 +3,13 @@ package com.epam.loops;
 public class MultiplicationTable {
 
     public void printTable(int numberTableToPrint) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.printf("%d x %d = %d\n", numberTableToPrint, i, numberTableToPrint * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        new MultiplicationTable().printTable(4);
     }
 
 }
--- src/main/java/com/epam/loops/Pyramid.java
+++ src/main/java/com/epam/loops/Pyramid.java
@@ -3,7 +3,18 @@ package com.epam.loops;
 public class Pyramid {
 
     public void printPyramid(int cathetusLength) {
-
+        for (int i = 1; i <= 5; i++) {
+            System.out.println();
+            for (int j = 1; j <= 5 - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j);
+            }
+        }
     }
 
     public static void main(String[] args) {
